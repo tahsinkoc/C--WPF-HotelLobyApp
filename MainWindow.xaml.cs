ec618e6aac7c3c9482592a146b34c63e8115a9e3
tahsinkoc <tahsinkoc6@gmail.com>
2022-05-26 10:02:34 +0300

    Innitial Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OtelWDB.Model;
+namespace hotel_loby
+{
+    /// <summary>
+    /// MainWindow.xaml etkileşim mantığı
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Otel _odam = new Otel();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            roomData.ItemsSource = _odam.Get(4);
+            roomData.SelectedCells.Clear();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void single_Click(object sender, RoutedEventArgs e)
+        {
+            roomData.ItemsSource = _odam.Get(1);
+            roomData.SelectedCells.Clear();
+        }
+
+        private void duo_Click(object sender, RoutedEventArgs e)
+        {
+            roomData.ItemsSource = _odam.Get(2);
+            roomData.SelectedCells.Clear();
+        }
+
+        private void tripple_Click(object sender, RoutedEventArgs e)
+        {
+            roomData.ItemsSource = _odam.Get(3);
+            roomData.SelectedCells.Clear();
+        }
+
+        private void roomData_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //object item = roomData.SelectedItem;
+            //string ID = (roomData.SelectedCells[0].Column.GetCellContent(item) as TextBlock).Text;
+
+            //var ID = (roomData.SelectedItem as Otel).odaNum;
+            //if (ID == null)
+
+            DataGrid gd = (DataGrid)sender;
+
+            Otel row_selected = gd.SelectedItem as Otel;
+            if (row_selected != null)
+            {
+                var ID = row_selected.odaNum;
+                keep.Content = $"{ID} NUMARALI ODAYI KİRALA";
+
+            }
+
+            //keep.Content = $"{ID} NUMARALI ODAYI KİRALA";
+        }
+
+        private void keep_Click(object sender, RoutedEventArgs e)
+        {
+            int geciciOda = (roomData.SelectedItem as Otel).odaNum;
+            _odam.odaNum = geciciOda;
+            _odam.odaOwnerName = customerName.Text;
+            _odam.odaOwnerSurName = customerSurname.Text;
+            _odam.odaStatus = "DOLU";
+            _odam.time = keepTime.Text;
+
+            _odam.Save(_odam);
+            roomData.ItemsSource = _odam.Get(4);
+            roomData.SelectedCells.Clear();
+
+            customerName.Text = " ";
+            customerSurname.Text = " ";
+            keepTime.Text = " ";
+
+            MessageBox.Show($"Oda Başarıyla {_odam.odaOwnerName} Adlı Müşteriye {_odam.time} Günlüğüne Kiralandı");
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            int geciciOda = (roomData.SelectedItem as Otel).odaNum;
+            MessageBox.Show($"{geciciOda.ToString()} Numaralı Oda Başarıyla Boşaltıldı.");
+
+            _odam.odaNum = geciciOda;
+            _odam.odaOwnerName = "BOŞ";
+            _odam.odaOwnerSurName = "BOŞ";
+            _odam.odaStatus = "BOŞ";
+            _odam.time = "BOŞ";
+
+            _odam.Save(_odam);
+            roomData.ItemsSource = _odam.Get(4);
+            roomData.SelectedCells.Clear();
+        }
+    }
+}
---
+++ Model/Otel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+
+namespace OtelWDB.Model
+{
+    class Otel
+    {
+        public int odaNum { get; set; }
+        public int odaCap { get; set; }
+        public string odaOwnerName { get; set; }
+        public string odaOwnerSurName { get; set; }
+        public string odaStatus { get; set; }
+        public string time { get; set; }
+
+        SQLiteConnection _baglan = new SQLiteConnection("Data Source =dbb/otel.db");
+        
+        //SqlConnection _baglan = new SqlConnection("Data Source=DESKTOP-68QGCQF;Initial Catalog=otel;Integrated Security=True");
+
+        public void connect()
+        {
+            if (_baglan.State == ConnectionState.Closed)
+            {
+                _baglan.Open();
+            }
+        }
+
+        public List<Otel> Get(int a)
+        {
+            connect();
+            SQLiteCommand command = null;
+            if (a == 4)
+            {
+                command = new SQLiteCommand($"select * from room_owner", _baglan);
+
+            }
+            else
+            {
+                command = new SQLiteCommand($"select * from room_owner where odaCap={a}", _baglan);
+            }
+
+
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            List<Otel> odalar = new List<Otel>();
+
+            while (reader.Read())
+            {
+                Otel oda = new Otel();
+
+                oda.odaNum = Convert.ToInt32(reader["odaNum"]);
+                oda.odaCap = Convert.ToInt32(reader["odaCap"]);
+                oda.odaOwnerName = reader["odaOwnerName"].ToString();
+                oda.odaStatus = reader["odaStatus"].ToString();
+                oda.odaOwnerSurName = reader["odaOwnerSurName"].ToString();
+                oda.time = reader["time"].ToString();
+
+                odalar.Add(oda);
+            }
+
+            reader.Close();
+            _baglan.Close();
+
+            return odalar;
+        }
+
+        public void Save(Otel mp)
+        {
+            connect();
+
+            SQLiteCommand command = new SQLiteCommand("Update room_owner set odaOwnerName=@odaOwnerName, odaStatus=@odaStatus, odaOwnerSurName=@odaOwnerSurName, time=@time where odaNum=@odaNum", _baglan);
+
+            command.Parameters.AddWithValue("@odaNum", Convert.ToInt32(mp.odaNum));
+            command.Parameters.AddWithValue("@odaOwnerName", mp.odaOwnerName.ToString());
+            command.Parameters.AddWithValue("@odaOwnerSurname", mp.odaOwnerSurName.ToString());
+            command.Parameters.AddWithValue("@odaStatus", mp.odaStatus.ToString());
+            command.Parameters.AddWithValue("@time", mp.time.ToString());
+
+            command.ExecuteNonQuery();
+            _baglan.Close();
+        }
+    }
+}
